54d49b05cb823210c7ee73262e79e80793e5fbb5
Rolf Kristensen <sweaty1@hotmail.com>
2023-06-08 20:45:09 +0200

    Updated to use NLog AsyncTaskTarget

--- src/NLog.Azure.Kusto/ADXTarget.cs
+++ src/NLog.Azure.Kusto/ADXTarget.cs
@@ -1,26 +1,27 @@
 ﻿using System;
-using Kusto.Ingest;
-using Kusto.Data;
-using NLog.Targets;
 using System.IO;
-using Microsoft.IO;
 using System.IO.Compression;
 using System.Text.Json;
+using Kusto.Ingest;
+using Kusto.Data;
 using Kusto.Data.Common;
+using Microsoft.IO;
 using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
 
 namespace NLog.Azure.Kusto
 {
     [Target("ADXTarget")]
-    public class ADXTarget : TargetWithLayout
+    public class ADXTarget : AsyncTaskTarget
     {
         ADXSinkOptions options;
         private IKustoIngestClient m_ingestClient;
         private IngestionMapping m_ingestionMapping;
         private bool m_disposed;
         private bool m_streamingIngestion;
-        private int m_ingestionTimeout; //seconds
         private static readonly RecyclableMemoryStreamManager SRecyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
+        private readonly JsonLayout _jsonLayoutProperties = new JsonLayout() { IncludeEventProperties = true, MaxRecursionLimit = 10 };
 
         [RequiredParameter]
         public string Database { get; set; }
@@ -37,8 +38,11 @@ namespace NLog.Azure.Kusto
         public string FlushImmediately { get; set; } = "false";
         public string MappingNameRef { get; set; }
 
-
-        public string IngestionTimout { get; set; }
+        public ADXTarget()
+        {
+            Layout = "${logger}|${message}";
+            IncludeEventProperties = true;
+        }
 
         protected override void InitializeTarget()
         {
@@ -57,7 +61,6 @@ namespace NLog.Azure.Kusto
             SetupAuthCredentials(options, defaultLogEvent);
             m_streamingIngestion = options.UseStreamingIngestion;
             m_ingestionMapping = new IngestionMapping();
-            m_ingestionTimeout = RenderLogEvent(IngestionTimout, defaultLogEvent) == "" ? 0 : int.Parse(RenderLogEvent(IngestionTimout, defaultLogEvent));
 
             if (!string.IsNullOrEmpty(options.MappingName))
             {
@@ -71,6 +74,27 @@ namespace NLog.Azure.Kusto
                 ? KustoIngestFactory.CreateManagedStreamingIngestClient(engineKcsb, dmkcsb)
                 : KustoIngestFactory.CreateQueuedIngestClient(dmkcsb);
 
+            _jsonLayoutProperties.IncludeEventProperties = IncludeEventProperties;
+            if (ContextProperties?.Count > 0)
+            {
+                _jsonLayoutProperties.Attributes.Clear();
+                foreach (var contextProperty in ContextProperties)
+                {
+                    _jsonLayoutProperties.Attributes.Add(new JsonAttribute(contextProperty.Name, contextProperty.Layout));
+                }
+            }
+        }
+
+        protected override void CloseTarget()
+        {
+            try
+            {
+                m_ingestClient?.Dispose();
+            }
+            finally
+            {
+                m_ingestClient = null;
+            }
         }
 
         private void SetupAuthCredentials(ADXSinkOptions options, LogEventInfo defaultLogEvent)
@@ -91,9 +115,10 @@ namespace NLog.Azure.Kusto
             }
         }
 
-        protected override async void Write(LogEventInfo logEvent)
+        protected override async Task WriteAsyncTask(IList<LogEventInfo> logEvents, CancellationToken cancellationToken)
         {
-            using var datastream = CreateStreamFromLogEvents(ADXLogEvent.GetADXLogEvent(logEvent, RenderLogEvent(Layout, logEvent)));
+            using var datastream = CreateStreamFromLogEvents(logEvents);
+
             var sourceId = Guid.NewGuid();
             IKustoIngestionResult result;
             if (m_streamingIngestion)
@@ -129,14 +154,26 @@ namespace NLog.Azure.Kusto
             }
         }
 
-        private Stream CreateStreamFromLogEvents(ADXLogEvent adxloginfo)
+        protected override Task WriteAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();    // Will never be hit, with override of WriteAsyncTask(IList<LogEventInfo> logEvents)
+        }
+
+        private Stream CreateStreamFromLogEvents(IList<LogEventInfo> batch)
         {
             var stream = SRecyclableMemoryStreamManager.GetStream();
-
             using (GZipStream compressionStream = new GZipStream(stream, CompressionMode.Compress, leaveOpen: true))
             {
-                JsonSerializer.Serialize(compressionStream, adxloginfo);
+                for (int i = 0; i < batch.Count; ++i)
+                {
+                    var logEvent = batch[i];
+                    var logEventMessage = RenderLogEvent(Layout, logEvent);
+                    var logEventJsonProperties = RenderLogEvent(_jsonLayoutProperties, logEvent).NullIfEmpty() ?? "{}";
+                    var adxloginfo = ADXLogEvent.GetADXLogEvent(logEvent, logEventMessage, logEventJsonProperties);
+                    JsonSerializer.Serialize(compressionStream, adxloginfo);
+                }
             }
+
             stream.Seek(0, SeekOrigin.Begin);
             return stream;
         }
@@ -159,12 +196,12 @@ namespace NLog.Azure.Kusto
         }
         #endregion
     }
-    public static class StringExtensions
+
+    internal static class StringExtensions
     {
         public static string NullIfEmpty(this string s)
         {
             return string.IsNullOrEmpty(s) ? null : s;
         }
     }
-
 }
